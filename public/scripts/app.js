dc5acbf03044b3d2ddbe6d0dbcd0e97f9b7e1d5a
Craig Rice <craigricetong@gmail.com>
2018-03-28 21:20:26 +0000

    added get and post routes and error messages

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,84 +1,94 @@
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
-
-
-// calls data array of tweets to render tweets
-function renderTweets(tweets) {
-    // loops through data array of tweets
-    for (var i = 0; i < tweets.length; i++) {
-        // pulls out each user to run through createTweetElement function
-        createTweetElement(tweets[i]);
-    }
-}
-
-// function to create a tweet article and populate it
-function createTweetElement(tweet) {
-    // creates a user variable for getting into tweetData object
-    let user = tweet.user
-
-    let date = Date(tweet.created_at)
-    // creates nested article for tweet posts and appends to section #all-tweets
-    let $tweet = $(  `<article>\
-                        <header>\
-                            <img class='userPic' src='${user.avatars.regular}'>\
-                            <div class='name'>${user.name}</div>\
-                            <div class='handle'>${user.handle}</div>\
-                        </header>\
-                        <p>${tweet.content.text}</p>\
-                        <footer>\
-                            ${Date(tweet.created_at)} days ago\
-                        </footer>\
-                    </article>` )
-    .appendTo( "#all-tweets" );
-    return $tweet
-}
-
-
+// function calls and handlers that happen after DOM is loaded
 $(document).ready(function() {
-    renderTweets(data);
+
+    // calls data array of tweets to render tweets
+    function renderTweets(tweets) {
+        // loops through data array of tweets
+        for (var i = 0; i < tweets.length; i++) {
+            // pulls out each user to run through createTweetElement function
+            createTweetElement(tweets[i]);
+        }
+    }
+
+    // function to create a tweet article and populate it
+    function createTweetElement(tweet) {
+        // creates a user variable for getting into tweetData object
+        let user = tweet.user
+
+
+    // var d = new Date(timestamp * 1000)
+        let date = new Date(tweet.created_at);
+
+        // In process to calculating days
+        // let currentDate = Date();
+
+        // creates nested article for tweet posts and appends to section #all-tweets
+        let $tweet = $(  `<article>
+                            <header>
+                                <img class='userPic' src='${user.avatars.regular}'>
+                                <div class='name'>${user.name}</div>
+                                <div class='handle'>${user.handle}</div>
+                            </header>
+                            <p>${tweet.content.text}</p>
+                            <footer>
+                                Originally Posted ${date}
+                            </footer>
+                        </article>` )
+        .appendTo( "#all-tweets" );
+        return $tweet
+    }
+
+    // Submit button function to stop normal behaviour
+    function onButtonClick(event) {
+        // stop normal submit page reload
+        event.preventDefault();
+        // creates variable for tweet text that is ready to send
+        let inputText = $('#content').serialize();
+
+        let ErrorCheck = $('#content').val();
+
+        console.log(ErrorCheck);
+
+        // Error Message on blank entry - The dirty bird method
+        if (ErrorCheck === '') {
+            $("#error").empty();
+            $(`<p>Give us your thoughts.</p>`).fadeIn('slow').appendTo( "#error" );
+            return ;
+        }
+        // Error Message if message over 140 characters - The dirty bird method
+        if (ErrorCheck.length > 140) {
+            $("#error").empty();
+            $(`<p>You have too much on your mind!</p>`).fadeIn('slow').appendTo( "#error" );
+            return ;
+        }
+
+        // Post to tweet json file
+        $.ajax({
+              url: "/tweets/",
+              method: "POST",
+              data: inputText
+        });
+        $('#content').val('');
+    }
+
+    // Loads tweets from json file
+    function loadTweets() {
+        // ajax call to get new json info
+        $.ajax({
+            url: "/tweets",
+            method: "GET",
+            dataType: "json",
+            complete: function (data_response) {
+                        let tweetsString = data_response.responseText;
+                        let tweetsArr = JSON.parse(tweetsString);
+                        // calls render tweet to the json Arr that is loaded
+                        renderTweets(tweetsArr);
+                    }
+        });
+    }
+
+    // calls function to load tweets from AJAX json call
+    loadTweets();
+    // handler for submitting the tweets
+    $( "#submitTweet" ).on('click', onButtonClick);
 });
